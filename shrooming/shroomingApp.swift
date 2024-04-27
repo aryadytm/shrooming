98d1cfe236c494d89e5462eb75ad519dbf691cfd
Arya Adyatma <88531676+aryadytm@users.noreply.github.com>
2024-04-27 12:52:20 +0700

    Initial Commit

---
+++ shrooming/shroomingApp.swift
@@ -0,0 +1,17 @@
+//
+//  shroomingApp.swift
+//  shrooming
+//
+//  Created by Arya Adyatma on 27/04/24.
+//
+
+import SwiftUI
+
+@main
+struct shroomingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
